b4df45d6c8596eb2182b16b26b98d782d22e78ce
Reinmic <maotoam@gloridauniversitaria.es>
2023-02-14 15:26:30 +0100

    atotal completado

--- atotal.sh
+++ atotal.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+
+ciudad=""
+total=0
+
+
+function total_ciudad {
+    total=0
+    for i in $(cat consumos.txt | grep $1 | cut -d" " -f3)
+    do
+        total=$(($total+$i))
+    done
+}
+
+
+while [ -z $ciudad ]
+do
+    read -p "Introduce una ciudad: " ciudad
+    total_ciudad $ciudad
+    if [ $total -eq 0 ]
+    then
+        echo "La ciudad no existe"
+        ciudad=""
+    fi
+done
+
+echo "El total de consumo de $ciudad es $total"
+
+
+
+
+
