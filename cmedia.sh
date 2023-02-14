a55ae10b64c3df554e8bf44e837fc49f8735eb88
Reinmic <maotoam@gloridauniversitaria.es>
2023-02-14 16:10:36 +0100

    Añado cmedia script

---
+++ cmedia.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+consumoT=0 
+
+apariciones=`cat consumos.txt|grep $1 | wc -l` 
+
+for i in `seq 1 $apariciones` 
+
+do 
+
+    consumo=`cat consumos.txt|grep $1| head -n$i | tail -n1| awk '{print $4}'` 
+
+    consumoT=$(($consumoT + $consumo)) 
+
+done 
+
+ 
+ 
+
+media=$(($consumoT/$apariciones)) 
+
+ 
+ 
+
+Echo "La media de la ciudad de $1 es $media" 
